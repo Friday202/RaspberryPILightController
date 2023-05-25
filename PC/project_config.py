b31e57702c33fa0ac0b077c39869d9f5a360a5a4
Friday202 <122792037+Friday202@users.noreply.github.com>
2023-05-25 13:38:24 +0200

    added project config

---
+++ PC/project_config.py
@@ -0,0 +1,13 @@
+# SSH communication:
+RP_IP = "XXX.XXX.X.XX"
+RP_USERNAME = "pi"
+RP_PASSWORD = "raspberry"
+RP_HOSTNAME = "yourhostname"
+
+# MQTT communication:
+MQTT_USERNAME = "yourusername"
+MQTT_PASSWORD = "yourpassword"
+
+# Name of remote file to start on raspberry PI
+SCRIPT_NAME = "test.py"
+
